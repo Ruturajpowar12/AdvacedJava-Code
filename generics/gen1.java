bae7ba67a2d5348b2043465887dc5df16e3b8fb9
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-09-02 08:18:35 +0530

    generic simple program

---
+++ generics/gen1.java
@@ -0,0 +1,31 @@
+class demo1 <T>{
+    T name;
+
+    public demo1( T name) {
+        this.name= name;
+    }
+     T showName(){
+       return name;
+     }
+
+}
+
+class gen1{
+    public static void main(String[] args) {
+        
+        //string
+        demo1<String> g = new demo1<>("Ruturaj");
+        String str = g.showName();
+        System.out.println("String:"+str); //Ruturaj
+
+        // Integer
+        demo1<Integer> i = new demo1<>(13);
+        int num = i.showName();
+        System.out.println("Integer :"+num);
+
+        //double
+        demo1<Double> d = new demo1<>(14.234);
+        double num2 = d.showName();
+        System.out.println("Double:"+num2);
+    }
+}
